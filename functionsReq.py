0e7a9f4f1c8f29d684a120ae02fd8e273016fa60
Haxintosh <92402488+Haxintosh@users.noreply.github.com>
2022-12-14 16:12:01 -0500

    Update functionsReq.py

--- functionsReq.py
+++ functionsReq.py
@@ -1,5 +1,7 @@
-import requests
+import random
 
+import requests
+import config
 def formatPayload(prompt, temp, token):
     defaultTemp = 1
     if token != '':
@@ -25,22 +27,43 @@ def formatPayload(prompt, temp, token):
     return payload
 
 
-def formatHeader(path):
-    key = open(path, 'r')
-    bearer = "Bearer " + key.read()
+def formatHeader():
+    bearer = "Bearer " + config.API_KEY
     headers = {
         "Authorization": bearer,
         "Content-Type": "application/json"
     }
     return headers
 
+def formatImgPayload(prompt, size):
+
+    sizeDict = {
+        256: "256x256",
+        512: "512x512",
+        1024: "1024x1024"
+    }
+
+    if size == 256 or 512 or 1024:
+        sizeFinal = sizeDict.get(size)
+    else:
+        sizeFinal = sizeDict.get(512)
+
+    payload = {
+    "prompt": prompt,
+    "n": 1,
+    "size": sizeFinal
+    }
+
+    print(payload)
+    return payload
 
 def sendIT(payload, headers):
     answer = requests.post(url = "https://api.openai.com/v1/completions", json=payload, headers=headers)
     return answer
 
-
-
+def sendITImg(payload, headers):
+    answer = requests.post(url = "https://api.openai.com/v1/images/generations", json=payload, headers=headers)
+    return answer
 def formatAnswer(answer):
     answerDict = answer.json()
     choices = answerDict.get("choices")[-1]
@@ -48,9 +71,18 @@ def formatAnswer(answer):
     print(text)
     return text
 
-'''
-payload = formatPayload("say this is a test", 1, 16)
-header = formatHeader("key.txt")
-answer = sendIT(payload, header)
-formatAnswer(answer)
-'''
+def formatImg(answer):
+    answerDict = answer.json()
+    choices = answerDict.get("data")[-1]
+    text = choices.get("url")
+    print(text)
+    return text
+
+def randomFooter():
+    footerDict = {
+        1 : "hello world",
+        2 : "honk!" # add your fancy footers here ;)
+    }
+    answer = footerDict.get(random.randint(1, len(footerDict)+1))
+    return answer
+
